1cf2081ecea453e65866e018ada874d9fee93bb2
7kaji <hirotonbuu@gmail.com>
2018-09-08 17:47:56 +0900

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,15 @@ import { connect } from 'react-redux';
 import App from '../components/App';
 import { minus, plus } from '../actions';
 
-const mapStateToProps = state => (
-  {
-    number: state.number,
-    title: state.title,
-  }
-);
+const mapStateToProps = (state) => {
+  const n = Math.sign(state.number) === -1 ? 0 : state.number;
+  return (
+    {
+      number: state.number,
+      title: `${state.title} ${'!'.repeat(n)}`,
+    }
+  );
+};
 
 const mapDispatchToProps = dispatch => (
   {
